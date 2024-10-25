1c1e3923f2a307202302b01e6cdd9f83549ad6a3
Jimmyh091 <jaimecaballero164@gmail.com>
2024-10-25 16:21:59 +0200

    todas las cosas

--- app/src/main/java/com/example/ejercicio_noseque_di_team3/actividad2_Ole.kt
+++ app/src/main/java/com/example/ejercicio_noseque_di_team3/actividad2_Ole.kt
@@ -10,7 +10,7 @@ class actividad2_Ole : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_actividad2_ole)
+        setContentView(R.layout.activity_ejercicio_6)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/ejercicio_noseque_di_team3/ejercicio_5.kt
+++ app/src/main/java/com/example/ejercicio_noseque_di_team3/ejercicio_5.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class scrollviu : AppCompatActivity() {
+class ejercicio_5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ejercicio_7)
+        setContentView(R.layout.activity_ejercicio5)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

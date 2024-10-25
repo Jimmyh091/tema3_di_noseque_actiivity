3d88bb4c3cb4f1ac0af7dc1b96e00ee17217fef6
Jimmyh091 <jaimecaballero164@gmail.com>
2024-10-25 15:59:55 +0200

    ej2

--- app/src/main/java/com/example/ejercicio_noseque_di_team3/ejercicio3.kt
+++ app/src/main/java/com/example/ejercicio_noseque_di_team3/ejercicio3.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LinearLayoutEj : AppCompatActivity() {
+class ejercicio3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ejercicio_1)
+        setContentView(R.layout.activity_ejercicio_3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
